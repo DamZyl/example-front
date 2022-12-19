82990794468cd663a2e20ca5744258f623a2f498
DamZyl <damianzylka@outlook.com>
2022-12-19 22:10:32 +0100

    Added readme

--- src/data-access-layer/api-client.ts
+++ src/data-access-layer/api-client.ts
@@ -1,4 +1,32 @@
 import { CommentApi, EnumApi } from '../api-types/api';
+import { Configuration } from '../api-types/configuration';
 
-export const commentApi = new CommentApi();
-export const enumApi = new EnumApi();
+const TIME_OUT_TIME = 5000;
+const basePath = 'https://localhost:7254';
+const baseHeaders = {
+  'Cache-Control': 'no-store',
+  'Content-Type': 'application/json',
+};
+
+const baseConfiguration = new Configuration({
+  basePath,
+  baseOptions: {
+    timeout: TIME_OUT_TIME,
+    headers: baseHeaders,
+  },
+});
+
+// const bearerConfigParams = (apiKey?: string): Configuration =>
+//   new Configuration({
+//     basePath,
+//     baseOptions: {
+//       timeout: TIME_OUT_TIME,
+//       headers: {
+//         ...baseHeaders,
+//         Authorization: `Bearer ${apiKey}`,
+//       },
+//     },
+//   });
+
+export const commentApi = new CommentApi(baseConfiguration);
+export const enumApi = new EnumApi(baseConfiguration);
